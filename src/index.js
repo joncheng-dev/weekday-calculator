06718a6b704adef6e4c72860fc5347577c6adf8e
Jonathan Cheng <jcheng461@gmail.com>
2022-06-03 00:17:59 +0900

    add user interface function that uses number returned from Date object method calculateDay to return day of week. This appends to html

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/styles.css";
 import Date from "./calculator.js";
 
+// User Interface Logic
+function displayDayOfWeek(number) {
+  // Displays to html a different day of week based on result.
+  if (number === 0) {
+    return "Saturday";
+  } else if (number === 1) {
+    return "Sunday";
+  } else if (number === 2) {
+    return "Monday";
+  } else if (number === 3) {
+    return "Tuesday";
+  } else if (number === 4) {
+    return "Wednesday";
+  } else if (number === 5) {
+    return "Thursday";
+  } else if (number === 6) {
+    return "Friday";
+  } else {
+    return "Error 101";
+  }
+}
+
 $(document).ready(function () {
   // To do
   $("#userInput").submit(function (event) {
@@ -12,7 +34,8 @@ $(document).ready(function () {
     const inputMonth = parseInt($("#month").val());
     const inputDay = parseInt($("#day").val());
     const dateIntoAlgorithm = new Date(inputYear, inputMonth, inputDay);
-    const dayOfWeek = dateIntoAlgorithm.calculateDay();
+    const codeForDayOfWeek = dateIntoAlgorithm.calculateDay();
+    const dayOfWeek = displayDayOfWeek(codeForDayOfWeek);
     $("#appResponse").append("<p>" + dayOfWeek + "</p>");
   });
 });
